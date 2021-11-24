52e063791a33d7f43e37b23672e1477699b26336
Ramtin Hafezinejad <Ramtin.Hafezinejad@gmail.com>
2021-11-24 23:36:08 +0100

    userinterface for quicksort created

--- src/script.js
+++ src/script.js
@@ -1,7 +1,18 @@
 function quicksort() {
     var chosenValueQuicksort = document.getElementById("number-quick").value;
-    console.log("User choose", chosenValueQuicksort, " number")
+    if (chosenValueQuicksort > 200) {
+        document.getElementById("quicksort-number").innerHTML = "Sorry, please choose no more than 200 random numbers to sort";
+        document.getElementById("number-quick").value = "";
+        document.getElementById("quicksort-array").textContent = "";
+    } else {
 
-
-    document.getElementById("quicksort-array").innerHTML = chosenValueQuicksort;
+        console.log("User choose", chosenValueQuicksort, "number")
+        document.getElementById("quicksort-number").innerHTML = "User has choosen " + chosenValueQuicksort + " random numbers (0 - 200) to sort: ";
+        let quicksortArray = Array.from({ length: chosenValueQuicksort }, () => Math.floor(Math.random() * 201));
+        let quicksortArray_String = quicksortArray.map(String);
+        console.log(quicksortArray_String);
+        document.getElementById("quicksort-array").textContent = quicksortArray_String;
+        document.getElementById("number-quick").value = ""
+    }
+    // document.getElementById("quicksort-array").innerHTML = "Array: " + quicksortArray;
 }
